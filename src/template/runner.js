d0e8d7dc00afc5eec17ecd9cd5c1d6753eadb5a0
robbinat <robbinat@umich.edu>
2023-12-19 16:53:19 -0500

    ignore spaces in url

--- src/template/runner.js
+++ src/template/runner.js
@@ -73,7 +73,8 @@ const turkGetParam = (name) => {
     name = name.replace(/[[]/, "\[").replace(/[]]/, "\]");
     var regexS = "[?&]" + name + "=([^&#]*)";
     var regex = new RegExp(regexS);
-    var results = regex.exec(window.location.href);
+    var url = window.location.href.replace('%20', '')
+    var results = regex.exec(url);
 
     if (results == null)
         return "";
